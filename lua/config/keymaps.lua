707fe91bd38aa62d0f542e2af9664978131e90ba
lerejoc <lerejoc@tirant.com>
2024-04-18 13:36:47 +0200

    Add command to boilerplate chai test

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- local json = require("json")
 vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 
@@ -10,3 +11,9 @@ vim.keymap.set('n', '<leader>fj', tbuiltin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function() tbuiltin.grep_string({ search = vim.fn.input('Grep >')}) end)
 
 vim.api.nvim_command('command! -range=% Formatjq <line1>,<line2>!jq .')
+
+local chai_test = "\z
+  \"import { expect } from \'chai\'\", \z
+  \"\", \z
+  \"describe(\'harness\', () => {})\""
+vim.api.nvim_command('command! Chaitest call append(line("."), [' .. chai_test .. '])')
