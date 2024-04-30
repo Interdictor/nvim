967330678f659e89628846523ba01fd9b2dc4736
Interdictor <lerejoc@gmail.com>
2024-04-30 09:49:20 +0200

    Install telescope-live-grep-args

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,3 +1,9 @@
 return {
-  'nvim-telescope/telescope.nvim', tag = '0.1.6', dependencies = { 'nvim-lua/plenary.nvim' },
+  'nvim-telescope/telescope.nvim', tag = '0.1.6', dependencies = {
+    { 'nvim-lua/plenary.nvim' },
+    { "nvim-telescope/telescope-live-grep-args.nvim", version = "^1.0.0" },
+  },
+  config = function ()
+    require("telescope").load_extension("live_grep_args")
+  end
 }
